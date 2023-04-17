0a726589c83d2b430fe2cdd20d7ec66f773b1750
Iqbal Attila <devas74tor@gmail.com>
2023-04-17 15:47:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,6 @@ def run_cmd(command):
     except KeyboardInterrupt:
         print("Process interrupted")
         sys.exit(1)
-run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P .")
-run_cmd("pip install basicsr")
-run_cmd("pip freeze")
-
-os.system("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth -P .")
 
 
 def inference(img,mode):
